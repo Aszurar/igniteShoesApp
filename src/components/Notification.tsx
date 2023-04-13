02cb255fc458eeeb56f89305bccb7e0d6ba937a1
Lucas De Lima Martins De Souza <lms.souza39@gmail.com>
2023-04-13 12:38:34 -0300

    :sparkles: Testando OneSignal: Recebendo dados por meio das notificações e exibindo notificação dentro do App em um componente personalizado :sparkles:

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,37 +1,58 @@
-import { HStack, Text, IconButton, CloseIcon, Icon } from 'native-base';
+import { HStack, Text, IconButton, CloseIcon, Icon, Pressable } from 'native-base';
 import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { OSNotification } from 'react-native-onesignal';
 
 type Props = {
-  title: string;
+  data: OSNotification;
   onClose: () => void;
 }
 
-export function Notification({ title, onClose }: Props) {
+type NotificationData = {
+  route: "details";
+  productId: string;
+}
+
+export function Notification({ data, onClose }: Props) {
+  const { navigate } = useNavigation();
+  const { route, productId } = data.additionalData as NotificationData;
+  console.log(data)
+  function handleNavigateToDetailsProduct() {
+    if (route === "details" && productId) {
+      navigate(route, { productId });
+      onClose();
+    }
+  }
+
   return (
-    <HStack 
-      w="full" 
-      p={4} 
+    <Pressable
+      w="full"
+      p={4}
       pt={12}
-      justifyContent="space-between" 
-      alignItems="center" 
       bgColor="gray.200"
       position="absolute"
       top={0}
+      onPress={handleNavigateToDetailsProduct}
     >
-        <Icon as={Ionicons} name="notifications-outline" size={5} color="black" mr={2}/>
+      <HStack
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Icon as={Ionicons} name="notifications-outline" size={5} color="black" mr={2} />
 
         <Text fontSize="md" color="black" flex={1}>
-          {title}
+          {data.title}
         </Text>
 
-      <IconButton 
-        variant="unstyled" 
-        _focus={{ borderWidth: 0 }} 
-        icon={<CloseIcon size="3" />} 
-        _icon={{ color: "coolGray.600"}} 
-        color="black"
-        onPress={onClose}
-      />
-    </HStack>
+        <IconButton
+          variant="unstyled"
+          _focus={{ borderWidth: 0 }}
+          icon={<CloseIcon size="3" />}
+          _icon={{ color: "coolGray.600" }}
+          color="black"
+          onPress={onClose}
+        />
+      </HStack>
+    </Pressable>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,16 +2,43 @@ import { useTheme } from 'native-base';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 
 import { AppRoutes } from './app.routes';
+import { useEffect, useState } from 'react';
+import OneSignal, { OSNotification } from 'react-native-onesignal';
+import { Notification } from '../components/Notification';
 
 export function Routes() {
   const { colors } = useTheme();
+  const [notification, setNotification] = useState<OSNotification>();
+
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
 
+
+
+  useEffect(() => {
+    const unsubscribe = OneSignal;
+
+    OneSignal.setNotificationWillShowInForegroundHandler((notificationReceivedEvent) => {
+      const response = notificationReceivedEvent.getNotification();
+      setNotification(response);
+    })
+
+    return () => unsubscribe;
+  }, []);
+
   return (
     <NavigationContainer theme={theme}>
       <AppRoutes />
+
+      {
+        notification && (
+          <Notification
+            data={notification}
+            onClose={() => setNotification(undefined)}
+          />
+        )
+      }
     </NavigationContainer>
   );
 }
