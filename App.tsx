db20c365909a728aea5a113a18e7815121076d9f
Lucas De Lima Martins De Souza <lms.souza39@gmail.com>
2023-04-13 12:37:44 -0300

    :sparkles: Testando OneSignal, dectando clique em botões :sparkles:

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -8,9 +9,34 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+import { useEffect } from 'react';
+const { ONE_SIGNAL_ID } = process.env;
+
+console.log("ONE_SIGNAL_ID", ONE_SIGNAL_ID)
+
+OneSignal.setAppId(ONE_SIGNAL_ID!);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+
+  useEffect(() => {
+    const unsubscribe = OneSignal.setNotificationOpenedHandler((response) => {
+      console.log(response);
+      const { actionId } = response.action as any;
+      switch (actionId) {
+        case '1':
+          return console.log('Ver todas');
+        case '2':
+          return console.log('Ver pedido');
+        default:
+          return console.log('Não foi clicado em botão de ação');
+      }
+    });
+
+    return unsubscribe;
+
+  }, []);
 
   return (
     <NativeBaseProvider theme={THEME}>
@@ -22,6 +48,7 @@ export default function App() {
       <CartContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
       </CartContextProvider>
+
     </NativeBaseProvider>
   );
 }
