ba7ebb24518857e5e765435782959a48d25a42a1
Lucas De Lima Martins De Souza <lms.souza39@gmail.com>
2023-04-20 21:32:12 -0300

    🏷️Adicionando tipagem para nova rota de página não encontrada e para a rota de tabroutes🏷️

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -2,7 +2,9 @@ export declare global {
   namespace ReactNavigation {
     interface RootParamList {
       cart: undefined;
+      tabRoutes: undefined;
       products: undefined;
+      notFound: undefined;
       details: { productId: string };
     }
   }
