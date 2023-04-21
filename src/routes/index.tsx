0bc9368b0a585c7f21cd0a3fb6d4cf24bf995de3
Lucas De Lima Martins De Souza <lms.souza39@gmail.com>
2023-04-20 21:25:28 -0300

    :sparkles:Adição da configuração de uri-scheme para o deep link e configuração do objeto linking com todas rotas possíveis de deep-link :sparkles:

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,44 +1,69 @@
+import React, { useEffect, useState } from 'react';
+
 import { useTheme } from 'native-base';
+
+import OneSignal, { OSNotification } from 'react-native-onesignal';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 
-import { AppRoutes } from './app.routes';
-import { useEffect, useState } from 'react';
-import OneSignal, { OSNotification } from 'react-native-onesignal';
 import { Notification } from '../components/Notification';
+import { TabRoutes } from './tab.routes';
+import { AppRoutes } from './app.routes';
+
+const linking = {
+  prefixes: [
+    'com.aszura.igniteshoesapp://',
+    'igniteShoesApp://',
+    'exp+igniteshoesapp://',
+  ],
+  config: {
+    screens: {
+      tabRoutes: {
+        path: 'tabRoutes',
+        screens: {
+          details: {
+            path: 'details/:productId',
+            parse: {
+              productId: (productId: string) => productId,
+            },
+          },
+          products: 'products',
+          cart: 'cart',
+        },
+      },
+      notFound: '*',
+    },
+  },
+};
 
 export function Routes() {
   const { colors } = useTheme();
   const [notification, setNotification] = useState<OSNotification>();
 
-
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
-
-
 
   useEffect(() => {
     const unsubscribe = OneSignal;
 
-    OneSignal.setNotificationWillShowInForegroundHandler((notificationReceivedEvent) => {
-      const response = notificationReceivedEvent.getNotification();
-      setNotification(response);
-    })
+    OneSignal.setNotificationWillShowInForegroundHandler(
+      notificationReceivedEvent => {
+        const response = notificationReceivedEvent.getNotification();
+        setNotification(response);
+      },
+    );
 
     return () => unsubscribe;
   }, []);
 
   return (
-    <NavigationContainer theme={theme}>
+    <NavigationContainer theme={theme} linking={linking}>
       <AppRoutes />
-
-      {
-        notification && (
-          <Notification
-            data={notification}
-            onClose={() => setNotification(undefined)}
-          />
-        )
-      }
+      {notification && (
+        <Notification
+          data={notification}
+          onClose={() => setNotification(undefined)}
+        />
+      )}
     </NavigationContainer>
   );
 }
