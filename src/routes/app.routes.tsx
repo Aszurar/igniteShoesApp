c6995d8d8f8a2de75b1d2fd00c1c34c9b7907d06
Lucas De Lima Martins De Souza <lms.souza39@gmail.com>
2023-04-20 21:33:33 -0300

    :recycle: Reestruturação do arquivo AppRoutes para renderizar 2 contextos de rotas: 1. TabRoutes => Rotas da aplciação 2. NotFound => Tela de erro, quando algum deep link esta errado :recycle:

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,50 +1,20 @@
-import { Ionicons, Feather } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react';
 
-import { useTheme } from 'native-base';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import { Cart } from '../screens/Cart';
-import { Home } from '../screens/Home';
-import { Details } from '../screens/Details';
+import { NotFound } from '../screens/NotFound';
+import { TabRoutes } from './tab.routes';
 
-const { Navigator, Screen } = createBottomTabNavigator();
+const { Navigator, Screen } = createStackNavigator();
 
 export function AppRoutes() {
-  const { colors, sizes } = useTheme();
-
   return (
     <Navigator
       screenOptions={{
         headerShown: false,
-        tabBarShowLabel: false,
-        tabBarActiveTintColor: colors.green[500],
-        tabBarInactiveTintColor: colors.gray[300],
-        tabBarStyle: {
-          borderTopWidth: 0,
-          backgroundColor: colors.gray[800]
-        },
       }}>
-      <Screen
-        name="products"
-        component={Home}
-        options={{
-          tabBarIcon: ({ color }) => <Ionicons name="md-home-outline" color={color} size={sizes[6]} />
-        }}
-      />
-
-      <Screen
-        name="cart"
-        component={Cart}
-        options={{
-          tabBarIcon: ({ color }) => <Feather name="shopping-bag" color={color} size={sizes[6]} />,
-        }}
-      />
-
-      <Screen
-        name="details"
-        component={Details}
-        options={{ tabBarButton: () => null }}
-      />
+      <Screen name="tabRoutes" component={TabRoutes} />
+      <Screen name="notFound" component={NotFound} />
     </Navigator>
-  )
+  );
 }
